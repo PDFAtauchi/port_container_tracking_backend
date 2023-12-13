f07a8b07bdff077ca0a700ca5cb8f142bd9e2bc1
PDFAtauchi <floresatauchi@gmail.com>
2023-12-12 22:19:58 -0500

    chore: init project

---
+++ src/main/java/com/practice/portcontainertrackingbackend/PortContainerTrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.portcontainertrackingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortContainerTrackingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortContainerTrackingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/portcontainertrackingbackend/PortContainerTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.portcontainertrackingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortContainerTrackingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
