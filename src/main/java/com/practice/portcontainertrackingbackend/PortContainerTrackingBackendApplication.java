7aa5b51d9c2eac597eaa2146e75eef2c1075d982
PDFAtauchi <floresatauchi@gmail.com>
2023-12-12 22:55:39 -0500

    build: add code formatter

--- src/main/java/com/practice/portcontainertrackingbackend/PortContainerTrackingBackendApplication.java
+++ src/main/java/com/practice/portcontainertrackingbackend/PortContainerTrackingBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortContainerTrackingBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortContainerTrackingBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PortContainerTrackingBackendApplication.class, args);
+    }
 }
