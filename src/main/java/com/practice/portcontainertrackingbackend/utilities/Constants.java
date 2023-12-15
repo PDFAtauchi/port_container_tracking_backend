0794faad61747fe07557083a1333d221c087e302
PDFAtauchi <floresatauchi@gmail.com>
2023-12-15 18:51:01 -0500

    feat: implementation container controllers

---
+++ src/main/java/com/practice/portcontainertrackingbackend/utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.practice.portcontainertrackingbackend.utilities;
+
+public class Constants {
+    public static final String BASE_URL = "/container";
+    public static final String CREATE_CONTAINER_URL = "/api/v1/create";
+    public static final String DETAIL_CONTAINER_URL = "/api/v1/detail/{containerId}";
+
+    private Constants() {}
+}
